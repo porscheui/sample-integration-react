0388171211de8010f50c1470516d6893f3962736
mhaegeleMHP <mhaegeleMHP@users.noreply.github.com>
2020-10-21 07:56:24 +0000

    Automated deployment of example (master): b7096e1c16365a33a5dcefdaa8da46946f248313

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sample-integration-react/precache-manifest.387d5d4a3c41aa9991770eaa6e4a5fa5.js"
+  "/sample-integration-react/precache-manifest.6baa1ba24e208376f632f8d15de4fbd4.js"
 );
 
 self.addEventListener('message', (event) => {
